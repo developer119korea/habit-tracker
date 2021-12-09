85f581100f03cf10d7ecf7aa4b8754154e1c2e00
Youngwan Cho <developer119korea@gmail.com>
2021-12-09 22:33:15 +0900

    해빗 트래커 기능 완성
    - 내비게이션 바 추가
    - 해빗 추가 기능 추가
    - 해빗 제거 기능 추가
    - 리셋 버튼 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,64 @@
 import './app.css';
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
 import Habits from './components/habits';
 
-function App() {
-  return <Habits />
-}
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ],
+  }
 
+  handleIncreament = habit => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  }
+
+  handleDecreament = habit => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  }
+
+  handleDelete = habit => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({ habits });
+  }
+
+  handleReset = () => {
+    const habits = this.state.habits.map(habit => {
+      habit.count = 0;
+      return habit;
+    })
+    this.setState({ habits });
+  }
+
+  handleAddHabit = name => {
+    const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
+    this.setState({ habits });
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar totalCount={this.state.habits.filter(item => item.count > 0).length} />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncreament}
+          onDecrement={this.handleDecreament}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAddHabit}
+          onReset={this.handleReset}
+        />
+      </>
+    );
+  }
+}
 export default App;
